83e370bbb5f453d18f0ef5355d925f7f938e5870
Alexander Andrews <kaibacorp444@gmail.com>
2017-06-22 13:12:30 -0400

    Added loops and arrays

---
+++ JavaScript/0.08_arrays.js
@@ -0,0 +1,22 @@
+
+var movies = ["Se7en", "The Breakfast Club", "Good Will Hunting", "The Neverending Story"]
+
+var nbaPlayersNumbers = [13, 23, 33, 13];
+
+var mixedArray = ["Broccoli", 20, true];
+
+console.log(movies[1])
+console.log(mixedArray[0])
+
+
+for(var i = 0; i < movies.length; i++){
+	console.log(movies[i])
+}
+
+
+
+var favResturants = ["Olive Garden", "Texas Steak House", "Tappanyaki"]
+favResturants.sort();
+for (var i = 0; i < favResturants.length; i++){
+	console.log(favResturants[i])
+}
