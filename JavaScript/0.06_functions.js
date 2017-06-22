f0d75a167e84d5ceb079a09e615ec343e49ecc03
Alexander Andrews <kaibacorp444@gmail.com>
2017-06-22 10:58:00 -0400

    Add functions

---
+++ JavaScript/0.06_functions.js
@@ -0,0 +1,60 @@
+//functions
+
+
+function hello(){
+	console.log("Hello!!!");
+}
+
+hello();
+
+
+
+
+
+
+
+var hey = function(){
+	console.log("hey!");
+}
+
+function name(){
+	console.log("Cameran");
+}
+name();
+
+
+function multiplication(){
+	var x = 10;
+	var y = 100;
+	console.log(x * y);
+}
+multiplication();
+
+function numberEntered(x){
+	console.log("The number entered is", x);
+}
+numberEntered(5);
+numberEntered(7);
+numberEntered(1000000);
+
+
+function addingNumbersThroughFunctions(x, y, z){
+	console.log(x + y + z)
+}
+addingNumbersThroughFunctions(17, 100000, 0)
+addingNumbersThroughFunctions(10, 3, 50);
+
+
+function fullName(x, y){
+	console.log(x + y)
+}
+fullName("Ben", " Dover");
+
+
+function petNameAndBreed(x, y){
+	var details = x + ", " + y
+	console.log(details)
+}
+petNameAndBreed("Cinnamon", "Labordor Retriever")
+
+
